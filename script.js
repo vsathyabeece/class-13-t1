d89db599c1c02a3fc7e6619a962e3c11c04eaf08
vsathyabeece <125301268+vsathyabeece@users.noreply.github.com>
2023-03-13 09:55:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+/*var ele=document.createElement("div");
+ele.innerHTML="this is div";
+container.setAttribute=("class","container");
+
+var ele=document.createElement("div");
+ele.innerHTML="this is div";
+row.setAttribute=("class","row");
+
+var ele=document.createElement("div");
+ele.innerHTML="this is div";
+col.setAttribute=("class","col-1");
+
+ele1.append(ele2);
+ele.append(ele1);
+document.body.append(ele);*/
+
+//Nested STRUCTURE
+/*var ele=document.createElement("div");
+ele.innerHTML="this is div";
+ele.setAttribute("class","main");
+
+var ele1=document.createElement("div");
+ele1.innerHTML="this is div";
+ele1.setAttribute("class","main1");
+
+//Parent.append(child)
+ele.append(ele1);
+document.body.append(ele);*/
+
+
+function foo(){
+    var res=document.getElementById("First").value;
+    console.log(res);
+    var res1=document.getElementById("Middle").value;
+    console.log(res1);
+    var res2=document.getElementById("Last").value;
+    console.log(res2);
+    var res3=document.getElementById("main").value;
+    console.log(res3);
+    var res4=document.getElementById("pass").value;
+    console.log(res4);
+
+
+
+
+    }
+
+
+
+
